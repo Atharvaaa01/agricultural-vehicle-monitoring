46f58cce6f198f9eaadbabddc632de567c75c9c1
Atharvaaa01 <156744535+Atharvaaa01@users.noreply.github.com>
2026-01-13 23:15:10 +0530

    test_plate.py updated

--- test_plate.py
+++ test_plate.py
@@ -2,8 +2,8 @@ import cv2
 from src.ocr_processor import extract_text
 from src.plate_color_detector import detect_color
 
-img = cv2.imread("dataset/images/train/truck.jpg")
-
+img = cv2.imread("dataset/test/images/00000002_jpg.rf.acd8487b67c4b8136f907732062cccb8.jpg")
+#C:\Users\ASUS\Desktop\agricultural-vehicle-monitoring\dataset\test\images\00000002_jpg.rf.acd8487b67c4b8136f907732062cccb8.jpg
 text = extract_text(img)
 color = detect_color(img)
 
